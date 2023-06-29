8f025c31f67cdf132ac528520fc154eafd173bbb
Palina Bril <venera_48@mail.ru>
2023-06-30 01:49:48 +0300

    project

---
+++ src/main/java/by/aws/projectsecond/ProjectSecondApplication.java
@@ -0,0 +1,13 @@
+package by.aws.projectsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/aws/projectsecond/ProjectSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package by.aws.projectsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
